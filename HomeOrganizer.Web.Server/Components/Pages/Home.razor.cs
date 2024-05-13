d10f3abbf89f1d93f8a8be48be4f21bcbf00f4e7
Dominik Siegele <siegele@sintara.at>
2024-05-13 19:36:29 +0200

    Added options to add fruits, meats and vegetables;

--- HomeOrganizer.Web.Server/Components/Pages/Home.razor.cs
+++ HomeOrganizer.Web.Server/Components/Pages/Home.razor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using HomeOrganizer.Logic;
 using HomeOrganizer.Logic.Models;
@@ -15,17 +17,30 @@ public partial class Home
     private bool _isButtonExplosion;
     private Storage _storage;
     private IStorageEntry[] _itemsWithMissingCount;
+    private readonly FruitType[] _fruitTypes;
+    private readonly IStorageManager _storageManager;
+    private FruitType _selectedFruitType;
+    private MeatType _selectedMeatType;
+    private VegetableType _selectedVegetableType;
+    private bool _isAddingFruit;
+    private bool _isAddingMeat;
+    private bool _isAddingVegetable;
+    private int _actualCount;
+    private int _preferredCount;
+    private readonly VegetableType[] _vegetableTypes;
+    private readonly MeatType[] _meatTypes;
 
     [Inject] 
     public IStorageRepository StorageRepository { get; set; }
 
-
-    private readonly IStorageManager _storageManager;
-
     public Home()
     {
         _storageManager = new StorageManager();
+        _fruitTypes = Enum.GetValues<FruitType>();
+        _vegetableTypes = Enum.GetValues<VegetableType>();
+        _meatTypes = Enum.GetValues<MeatType>();
     }
+
     async Task LoadStorage()
     {
         _isLoading = true;
@@ -65,6 +80,56 @@ public partial class Home
     private void Decrease(IStorageEntry entry)
     {
         entry.ActualCount--;
+        StorageRepository.SaveStorage(_storage);
+    }
+
+    private void ShowAddFruit()
+    {
+        _isAddingFruit = true;
+        _isAddingMeat = false;
+        _isAddingVegetable = false;
+    }
+
+    private void ShowAddMeat()
+    {
+        _isAddingMeat = true;
+        _isAddingVegetable = false;
+        _isAddingFruit = false;
+    }
+
+    private void ShowAddVegetable()
+    {
+        _isAddingVegetable = true;
+        _isAddingMeat = false;
+        _isAddingFruit = false;
+    }
+
+    private void AddFruitEntry()
+    {
+        AddToEntries(new Fruit(_selectedFruitType));
+    }
+
+    private void AddVegetableEntry()
+    {
+        AddToEntries(new Vegetables(_selectedVegetableType));
+    }
+
+    private void AddMeatEntry()
+    {
+        AddToEntries(new Meat(_selectedMeatType));
+    }
+
+    private void AddToEntries(IStorageItem item)
+    {
+        var currentEntries = _storage.StorageEntries.ToList();
+        currentEntries.Add(new StorageEntry
+        {
+            StorageItem = item,
+            ActualCount = _actualCount,
+            PreferredCount = _preferredCount
+        });
+        _storage.StorageEntries = currentEntries.ToArray();
+
         StorageRepository.SaveStorage(_storage);
     }
 }
